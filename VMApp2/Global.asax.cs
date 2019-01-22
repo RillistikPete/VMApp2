78f670c5266db6d77751aa2c8359cfda75ccc6d4
Pete Forrest <petergforrest91@gmail.com>
2019-01-22 15:58:45 -0600

    created MappingProfile, added referene in AppStart method in global.axax.cs, after installing AutoMapper. Changed API/CustomersController.cs to include data transfer objects, set mapping for requests data

--- VMApp2/Global.asax.cs
+++ VMApp2/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using VMApp2.App_Start;
 
 namespace VMApp2
 {
@@ -13,6 +14,7 @@ namespace VMApp2
     {
         protected void Application_Start()
         {
+            AutoMapper.Mapper.Initialize(cfg => cfg.AddProfile<MappingProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
