d9282d52fbc7b9d546224a300561405d6662ce6b
Pete Forrest <petergforrest91@gmail.com>
2019-01-23 17:50:25 -0600

    modified WebApiConfig.cs, added to api/custmersController.cs, changed customerDTO

--- VMApp2/DTOs/CustomerDTO.cs
+++ VMApp2/DTOs/CustomerDTO.cs
@@ -15,10 +15,12 @@ namespace VMApp2.DTOs
         [StringLength(100)]
         public string Name { get; set; }
 
-        [Min18YrsIfMember]
+        //[Min18YrsIfMember]
         public DateTime? BirthDate { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
 
         public byte MembershipTypeId { get; set; }
     }
